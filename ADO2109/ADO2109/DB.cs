454c4f0e6758f088bbcf92f91ea8fa06036e2142
Viryz <viryz1998@gmail.com>
2018-10-04 00:10:45 +0300

    done

---
+++ ADO2109/ADO2109/DB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO2109
+{
+    public class DB
+    {
+        private SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=E:\УНІВЄР\ШАГ\C#\SharpADO2109\ADO2109\ADO2109\Groups.mdf;Integrated Security=True");
+
+        public void InsertGroup(string name)
+        {
+            connection.Open();
+            string insertString = "insert into Gruppa(Name) Values(@name)";
+            SqlCommand command = new SqlCommand(insertString, connection);
+            SqlParameter param = new SqlParameter("@name", name);
+            command.Parameters.Add(param);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public DataTable GetTable(string tableName)
+        {
+            connection.Open();
+            string selectionString = "select * from " + tableName;
+            SqlCommand command = new SqlCommand(selectionString, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+
+    }
+}
---
+++ ADO2109/ADO2109/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO2109
+{
+    public partial class Form1 : Form
+    {
+        DB db = new DB();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("TextBox is empty");
+                return;
+            }
+            db.InsertGroup(textBox1.Text);
+            dataGridView1.DataSource = db.GetTable("Gruppa");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.GetTable("Gruppa");
+        }
+    }
+}
